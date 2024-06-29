4c9c028bec88d9e6856619e7307b15f37cf1809d
Jaime Junr <jaimeodairbassojuniorjaime@gmail.com>
2024-06-28 23:51:03 -0300

    :lipstick: feat: Make Header Responsive for Mobile Devices

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,38 +9,61 @@ import { hadleOpen } from '../../store/reducers/cartSlice'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import { useState } from 'react'
 
 export default function Header() {
   const dispatch = useDispatch()
 
   const { items } = useSelector((state: RootReducer) => state.cart)
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <div className="container">
       <S.HeaderBar>
-        <div>
-          <Link to="/">
-            <img src={logo} alt="Eplay" />
-          </Link>
-
-          <nav>
-            <S.Links>
-              <S.LinkItem>
-                <Link to="/categories">Categorias</Link>
-              </S.LinkItem>
-              <S.LinkItem>
-                <a href="#">Novidades</a>
-              </S.LinkItem>
-              <S.LinkItem>
-                <a href="#">Promoções</a>
-              </S.LinkItem>
-            </S.Links>
-          </nav>
-        </div>
-        <S.CartButton onClick={() => dispatch(hadleOpen())}>
-          {items.length} - produto(s){' '}
-          <img src={carrinho} alt="Carrinho de Compras" />
-        </S.CartButton>
+        <S.HeaderRow>
+          <div>
+            <S.Hamburguer onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <span />
+              <span />
+              <span />
+            </S.Hamburguer>
+            <Link to="/">
+              <img src={logo} alt="Eplay" />
+            </Link>
+            <nav>
+              <S.Links>
+                <S.LinkItem>
+                  <Link to="/categories">Categorias</Link>
+                </S.LinkItem>
+                <S.LinkItem>
+                  <a href="#">Novidades</a>
+                </S.LinkItem>
+                <S.LinkItem>
+                  <a href="#">Promoções</a>
+                </S.LinkItem>
+              </S.Links>
+            </nav>
+          </div>
+          <S.CartButton onClick={() => dispatch(hadleOpen())}>
+            {items.length}
+            <span>- produto(s)</span>
+            <img src={carrinho} alt="Carrinho de Compras" />
+          </S.CartButton>
+        </S.HeaderRow>
+        <S.NavMobile className={isMenuOpen ? 'is-open' : ''}>
+          <S.Links>
+            <S.LinkItem>
+              <Link to="/categories">Categorias</Link>
+            </S.LinkItem>
+            <S.LinkItem>
+              <a href="#">Novidades</a>
+            </S.LinkItem>
+            <S.LinkItem>
+              <a href="#">Promoções</a>
+            </S.LinkItem>
+          </S.Links>
+        </S.NavMobile>
       </S.HeaderBar>
     </div>
   )
